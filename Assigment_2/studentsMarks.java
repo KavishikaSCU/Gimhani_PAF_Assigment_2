d8cacc3038a0323c9f9549e14ebdcfeccb8bc6ce
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2023-09-18 21:23:08 +0800

    commit 1

---
+++ Assigment_2/studentsMarks.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author (M.G.Gimhani Kavishika)
+ * @Student ID (24097251)
+ * @Date (18/9/2023)
+ * @Description - Assigment 2 for PROG5001-Fundamentals of Programming. Developed a simple program to compute 
+ * statistics of 'students' marks in this assignment.
+ */
+public class studentsMarks{
+    public static void main(String[]args){
+   try{
+       File myFile = new File("prog5001_students_grade_2022");
+       Scanner myReader = new Scanner(myFile);
+       //while(myReader.hasNextLine()){
+           //String line = 
+       }
+   //}
+   catch (FileNotFoundException e){
+           System.out.println("File doesnot found");
+       }
+//}
+
+//Function No 1 read the unit name from the given file
+//public static String unitName(String myFile){
+   // FileInput fileInput = new FileInput(myFile);
+    
+    
+    
+    
+    
+}
+}
+
