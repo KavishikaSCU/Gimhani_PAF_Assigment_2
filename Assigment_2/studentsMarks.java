f8eadf9b2a28a96ced7f5137da1809cd56ae3962
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2023-09-21 15:09:04 +0800

    commit 2

--- Assigment_2/studentsMarks.java
+++ Assigment_2/studentsMarks.java
@@ -1,5 +1,8 @@
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author (M.G.Gimhani Kavishika)
@@ -9,27 +12,78 @@ import java.io.*;
  * statistics of 'students' marks in this assignment.
  */
 public class studentsMarks{
+    //studentMarks properties declared as private
+    private String studentName;
+    private String studentId;
+    private String unitName;
+    private List<Integer>marks;
+    
+    public studentsMarks(String studnetName,String studentId,String unitName,List<Integer>marks){
+        this.studentName = studentName;
+        this.studentId = studentId;
+        this.unitName = unitName;
+        this.marks = marks;
+    }
+    
+    
+    
     public static void main(String[]args){
-   try{
-       File myFile = new File("prog5001_students_grade_2022");
-       Scanner myReader = new Scanner(myFile);
-       //while(myReader.hasNextLine()){
-           //String line = 
+       Scanner myReader = new Scanner(System.in);
+       System.out.println("Please enter your file name :");
+       String myFile = myReader.nextLine();
+       
+       List<studentsMarks>students = readMarks(myFile);
+       
+       //Print the data
+       for(studentsMarks student :students){
+           
        }
-   //}
-   catch (FileNotFoundException e){
-           System.out.println("File doesnot found");
-       }
-//}
+    }
 
-//Function No 1 read the unit name from the given file
-//public static String unitName(String myFile){
-   // FileInput fileInput = new FileInput(myFile);
-    
-    
-    
-    
-    
-}
+
+//Function No 1 read the marks from the given file
+public static List<studentsMarks>readMarks(String myFile){
+    List<studentsMarks> students = new ArrayList<>();
+    try{
+        File file = new File(myFile);
+        Scanner input = new Scanner(myFile);
+        
+        String studentName = null;
+        String studentId = null;
+        String unitName = null;
+        List<Integer> marks = new ArrayList<>();
+        
+        while(input.hasNextLine()){
+            String lines = input.nextLine();
+            
+            //check comment lines 
+            if(lines.startsWith("#")){
+                continue;
+            }
+            
+           String[] sections = lines.split(",");
+                if (sections.length == 4) {
+                    unitName = sections[0].trim();
+                    studentName = sections[1].trim();
+                    studentId = sections[2].trim();
+                    marks = new ArrayList<>();
+                    String[] marksArray = sections[3].trim().split(" ");
+                    for (String mark : marksArray) {
+                        marks.add(Integer.parseInt(mark));
+                    } 
+                     students.add(new studentsMarks(unitName, studentName, studentId, marks));
+        }
+    }
+        input.close();
+    } //catch(FileNotFoundException e){
+                //System.out.println("File doesnot found");
+                //System.exit(1);
+           // }
+    return students;
 }
 
+
+        
+
+
+}
